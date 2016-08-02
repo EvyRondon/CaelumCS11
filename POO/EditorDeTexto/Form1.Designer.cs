f31d97cc9f815366c770cb2178f0d565f40cf14c
Evelym <Evelym@DESKTOP-4510DEV>
2016-08-02 12:10:55 -0400

    22.System.IO

---
+++ POO/EditorDeTexto/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace EditorDeTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConteudo = new System.Windows.Forms.TextBox();
+            this.btnGrava = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtConteudo
+            // 
+            this.txtConteudo.Location = new System.Drawing.Point(12, 12);
+            this.txtConteudo.Multiline = true;
+            this.txtConteudo.Name = "txtConteudo";
+            this.txtConteudo.Size = new System.Drawing.Size(260, 199);
+            this.txtConteudo.TabIndex = 0;
+            // 
+            // btnGrava
+            // 
+            this.btnGrava.Location = new System.Drawing.Point(12, 226);
+            this.btnGrava.Name = "btnGrava";
+            this.btnGrava.Size = new System.Drawing.Size(75, 23);
+            this.btnGrava.TabIndex = 1;
+            this.btnGrava.Text = "Gravar";
+            this.btnGrava.UseVisualStyleBackColor = true;
+            this.btnGrava.Click += new System.EventHandler(this.btnGrava_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnGrava);
+            this.Controls.Add(this.txtConteudo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConteudo;
+        private System.Windows.Forms.Button btnGrava;
+    }
+}
+
---
+++ POO/EditorDeTexto/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EditorDeTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("texto.txt"))
+            {
+                using (Stream entrada = File.Open("texto.txt", FileMode.Open))
+                using (StreamReader leitor = new StreamReader(entrada))
+                {
+                    string linha = leitor.ReadToEnd();
+                    while (linha != null)
+                    {
+                        txtConteudo.Text += linha;
+                        linha = leitor.ReadLine();
+                    }
+                }
+            }
+        }
+
+        private void btnGrava_Click(object sender, EventArgs e)
+        {
+            using (Stream saida = File.Open("texto.txt", FileMode.Create))
+            using (StreamWriter escritor = new StreamWriter(saida))
+            {
+                escritor.Write(txtConteudo.Text);
+            }
+        }
+    }
+}
