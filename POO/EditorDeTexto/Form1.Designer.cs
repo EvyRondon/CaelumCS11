4b084e22f3e3fb5809895640ff1c554684392616
29888 <29888@CTJ-DMP-860>
2016-10-06 14:09:10 -0400

    Modificações do ultimo capitulo

---
+++ POO/EditorDeTexto/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace EditorDeTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textoConteudo = new System.Windows.Forms.TextBox();
+            this.btnGrava = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textoConteudo
+            // 
+            this.textoConteudo.Location = new System.Drawing.Point(12, 12);
+            this.textoConteudo.Multiline = true;
+            this.textoConteudo.Name = "textoConteudo";
+            this.textoConteudo.Size = new System.Drawing.Size(260, 199);
+            this.textoConteudo.TabIndex = 0;
+            // 
+            // btnGrava
+            // 
+            this.btnGrava.Location = new System.Drawing.Point(12, 217);
+            this.btnGrava.Name = "btnGrava";
+            this.btnGrava.Size = new System.Drawing.Size(75, 23);
+            this.btnGrava.TabIndex = 1;
+            this.btnGrava.Text = "Gravar";
+            this.btnGrava.UseVisualStyleBackColor = true;
+            this.btnGrava.Click += new System.EventHandler(this.btnGrava_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnGrava);
+            this.Controls.Add(this.textoConteudo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textoConteudo;
+        private System.Windows.Forms.Button btnGrava;
+    }
+}
+
